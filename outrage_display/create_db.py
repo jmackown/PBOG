0533d406f2e9d38d702f793c6b5de447be2a32c3
Jennifer Mackown <jen@scholarpack.com>
2018-11-10 16:08:15 +0000

    added a load of local paper urls to db create

--- outrage_display/create_db.py
+++ outrage_display/create_db.py
@@ -2,7 +2,11 @@ from database import db
 from flask import Blueprint
 from database.models import URLList, ScrapedData
 
+import url_master
 ragnarok_bp = Blueprint('ragnarok', __name__)
+
+# url_master_list = {'abingdon+herald': 'www.heraldseries.co.uk',
+#                    'accrington+observer': 'www.accringtonobserver.co.uk'}
 
 
 @ragnarok_bp.route('/ragnarok/', methods=['GET'])
@@ -11,6 +15,23 @@ def ragnarok():
     db.session.close()
     db.drop_all()
     db.create_all()
+
+
+    for name, url in url_master.url_master_list.items():
+        print(name)
+
+        new_url = URLList(
+            name = name,
+            url = url
+        )
+
+
+        db.session.add(new_url)
+
+
     db.session.commit()
+
+
+
 
     return "NUKED!! ☢ (and rebuilt database)"
--- outrage_display/database/models.py
+++ outrage_display/database/models.py
@@ -7,6 +7,7 @@ class URLList(db.Model):
     __tablename__ = 'url_lists'
 
     url_id = db.Column(db.Integer, primary_key=True, autoincrement=True)
+    name = db.Column(db.Text)
     url = db.Column(db.Text)
 
     def __repr__(self):
