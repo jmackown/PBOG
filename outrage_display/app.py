02537ba6e9d5e3666abfaeea9e279fe0ceacbbd0
Jennifer Mackown <jen@scholarpack.com>
2018-11-10 12:50:15 +0000

    added DB connection

--- outrage_display/app.py
+++ outrage_display/app.py
@@ -1,9 +1,27 @@
 from flask import Flask
-app = Flask(__name__)
+from flask_sqlalchemy import SQLAlchemy
 
-@app.route('/')
-def hello_world():
-    return 'Flask Dockerized'
+from database import db
+
+def create_app():
+
+    app = Flask(__name__)
+
+# app = Flask(__name__)
+
+    @app.route('/')
+    def hello_world():
+        return 'Flask Dockerized'
+
+
+    db.init_app(app)
+
+
+    return app
+
+if __name__ == '__main__':
+    app = create_app()
+    app.run(debug=True,host='0.0.0.0', port='5000')
 
 if __name__ == '__main__':
     app.run(debug=True,host='0.0.0.0', port='5000')
