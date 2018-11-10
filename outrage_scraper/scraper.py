a411a786c625f267a7c5232b7d0f13da7454bc83
Jay Turner <jayturner1832@gmail.com>
2018-11-10 18:08:56 +0000

    Scraper adds headline to the database

--- outrage_scraper/scraper.py
+++ outrage_scraper/scraper.py
@@ -6,10 +6,30 @@ import psycopg2
 class Scraper:
 
     def __init__(self):
-        self.websites = ("https://thelincolnite.co.uk/",)
+
         self.website_data = {}
+        self.websites = set(["https://thelincolnite.co.uk/", ])
+
+    def update_urls(self):
+        print("Updating URLS")
+        conn = psycopg2.connect(dbname="outrage", user="postgres", host="outrage_db", port='5432')
+
+        cur = conn.cursor()
+        cur.execute("SELECT url FROM url_lists;")
+
+        for row in cur.fetchall():
+            url = row[0]
+
+            if 'http://' not in url:
+                url = 'http://' + url
+
+            if url and url != '':
+                self.websites.add(url)
+
+        conn.close()
 
     def scrape(self):
+        self.update_urls()
         self.get_content_in_sites()
         self.get_header_tags()
 
@@ -18,30 +38,47 @@ class Scraper:
 
         for site in self.website_data:
             for headline in self.website_data[site]['headers']:
+                headline = headline.replace("'", "''")
+
+                print(f"Inserting {headline} for {site}")
+
                 sql = f"INSERT INTO scraped_data (headline, scrape_time) SELECT '{headline}', now() " \
                       f"WHERE NOT EXISTS (SELECT 1 FROM scraped_data WHERE headline = '{headline}');"
 
-                cur.execute(sql)
+                try:
+                    cur.execute(sql)
+                except psycopg2.ProgrammingError as e:
+                    print(e)
 
+        print("Commiting!")
         conn.commit()
+
         conn.close()
 
         return self.website_data
 
     def get_content_in_sites(self):
+        print("Getting Content")
 
         for website in self.websites:
 
             self.website_data[website] = {}
 
-            r = requests.get(website)
+            print(f"Getting content for {website}")
+
+            try:
+                r = requests.get(website)
+            except:
+                print(f"FAILED: {website}")
 
             if r.status_code == 200:
                 self.website_data[website]['content'] = r.text
             else:
+                del self.website_data[website]
                 print(f"Failed to access {website}")
 
     def get_header_tags(self):
+        print("Getting Headers")
 
         header_numbers = (1, 2, 3)
 
